82a9023a6e5146b6fca74d0319c25253fcc412df
Кубраков Глеб <kubrakov.gleb1@mail.ru>
2024-09-04 16:38:42 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -2,4 +2,9 @@ from flask import Flask
 app= Flask(__name__)
 @app.route("/")
 def start():
-    return "web-сервер на Flask"
+    return "<!doctype html>" \
+        "<html>" \
+        "    <body>" \
+        "        <h1>web-сервер на flask</h1>" \
+        "    </body>" \
+        "</html>"
