b5f31981c16d6bfe94bba1b289aa415cd138de42
Кубраков Глеб <kubrakov.gleb1@mail.ru>
2024-09-04 17:02:23 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,27 @@ from flask import Flask
 app= Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
     return """<!doctype html>
         <html> 
             <body>
-                <h1>web-сервер на flask</h1> 
+                <h1>web-сервер на flask</h1>
+                <a href="/author">author</a>
+            </body> 
+        </html>"""
+
+@app.route("/author")
+def author():
+    name="Кубраков Глеб Евгеньевич"
+    group= "ФБИ-22"
+    faculty="ФБ"
+    return """<!doctype html>
+        <html> 
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a>
             </body> 
         </html>"""
