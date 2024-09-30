bbf2f42ffe3329cc506cbd749aaae659a3b48de1
Кубраков Глеб <kubrakov.gleb1@mail.ru>
2024-09-30 23:04:54 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect, render_template_string, request
+from flask import Flask, url_for, redirect, render_template_string, request, render_template
 
 app = Flask(__name__)
 
@@ -375,4 +375,7 @@ def add_flower(name):
     </body>
 </html>
 '''
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')
 
