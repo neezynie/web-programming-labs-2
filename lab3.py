39c046e603785a71b7325033b43c20c4cf7a254a
Кубраков Глеб <kubrakov.gleb1@mail.ru>
2024-10-22 17:33:34 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,11 +1,23 @@
-from flask import Blueprint, url_for, redirect, render_template_string, request, render_template
+from flask import Blueprint, url_for, redirect, render_template_string, request, render_template, make_response
 
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
-def lab3_index():
-    return render_template('lab3/lab3.html')
+def lab():
+    name = request.cookies.get('name')
+    name_color = request.cookies.get('name_color') or 'black'  # Default to black if no color is set
+    return render_template('lab3/lab3.html', name=name, name_color=name_color)
 
 @lab3.route('/lab3/cookie')
-def lab3_cookie():
-    return "This is the cookie page for lab3."
+def cookie():
+    resp = make_response("Установка cookie", 200)
+    resp.set_cookie('name', 'Alex', max_age=5)
+    resp.set_cookie('name_color', 'blue', max_age=5)  # Set color to blue for 5 seconds
+    return resp
+
+@lab3.route('/lab3/del_cookie')
+def del_cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.delete_cookie('name')
+    resp.delete_cookie('name_color')
+    return resp
