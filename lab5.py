657db3bacc2e2283dad12c7c7ed6500e3f6a1bd1
Кубраков Глеб <kubrakov.gleb1@mail.ru>
2024-11-13 18:04:51 +0700

    Улучшение

--- lab5.py
+++ lab5.py
@@ -8,6 +8,21 @@ lab5 = Blueprint('lab5', __name__)
 def lab():
     return render_template('lab5/lab5.html', login=session.get('login'))
 
+def db_connect():
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'gleb_kubrakov_knowledge_base',
+        user = 'gleb_kubrakov_knowledge_base',
+        password = '123'
+    )
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
 @lab5.route('/lab5/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -19,13 +34,7 @@ def login():
     if not (login or password):
         return render_template('lab5/login.html', error='Заполните все поля')
     
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'gleb_kubrakov_knowledge_base',
-        user = 'gleb_kubrakov_knowledge_base',
-        password = '123'
-    )
-    cur = conn.cursor(cursor_factory=RealDictCursor)
+    conn, cur = db_connect()
     cur.execute(f"SELECT * FROM users WHERE login='{login}';")
     user = cur.fetchone()
 
@@ -55,24 +64,15 @@ def register():
 
     if not (login or password):
         return render_template('lab5/register.html', error='Заполните все поля')
-
-    conn = psycopg2.connect(
-        host = '127.0.0.1',
-        database = 'gleb_kubrakov_knowledge_base',
-        user = 'gleb_kubrakov_knowledge_base',
-        password = '123'
-    )
-    cur = conn.cursor()
+    
+    conn, cur = db_connect() 
     cur.execute(f"SELECT login From users Where login='{login}';")
     if cur.fetchone():
-        cur.close()
-        conn.close()
+        db_close(conn, cur)
         return render_template('lab5/register.html',
                                error='Такой пользователь уже существует')
     cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}','{password}');")
-    conn.commit()
-    cur.close()
-    conn.close()
+    db_close(conn, cur)
     return render_template('lab5/success_register.html', login=login)
 @lab5.route('/lab5/list')
 def list_articles():
