8751fc4986ba96b8b897808985e10b8ec1f43eec
Кубраков Глеб <kubrakov.gleb1@mail.ru>
2024-12-10 19:53:58 +0700

    Заготовка к 7-й лаборатороной

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'GLEB')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
@@ -18,6 +19,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 @app.errorhandler(404)
 def not_found(err):
     image_path = url_for('static', filename='404.jpg')
