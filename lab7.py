1e4e7f4ce8f38f1c97a463181581712cc36d62d7
Кубраков Глеб <kubrakov.gleb1@mail.ru>
2024-12-10 20:20:55 +0700

    Удаление выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -42,3 +42,10 @@ def get_films(id):
         return jsonify({"error": "Film not found"}), 404
     
     return jsonify(films[id])
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
+def del_film(id):
+    if id < 0 or id >= len(films):
+        return jsonify({"error": "Film not found"}), 404
+    del films[id]
+    return "", 204
